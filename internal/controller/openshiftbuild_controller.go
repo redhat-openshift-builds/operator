5d244b17d8ff5d71e466f74a11f1010bb5b25ae2
Sayan Biswas <sayan-biswas@live.com>
2024-07-03 06:26:02 +0530

    Add Shipwright Build controller

--- internal/controller/openshiftbuild_controller.go
+++ internal/controller/openshiftbuild_controller.go
@@ -18,21 +18,28 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
-	"k8s.io/apimachinery/pkg/api/errors"
-
-	"k8s.io/apimachinery/pkg/runtime"
+	"github.com/redhat-openshift-builds/operator/internal/common"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiruntime "k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
-	operatorv1alpha1 "github.com/redhat-openshift-builds/operator/api/v1alpha1"
+	openshiftv1alpha1 "github.com/redhat-openshift-builds/operator/api/v1alpha1"
+	shipwrightv1alpha1 "github.com/shipwright-io/operator/api/v1alpha1"
 )
 
 // OpenShiftBuildReconciler reconciles a OpenShiftBuild object
 type OpenShiftBuildReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+	Client client.Client
+	Scheme *apiruntime.Scheme
 }
 
 //+kubebuilder:rbac:groups=operator.openshift.io,resources=openshiftbuilds,verbs=get;list;watch;create;update;patch;delete
@@ -41,31 +48,120 @@ type OpenShiftBuildReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *OpenShiftBuildReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx).WithValues("name", req.Name)
 	logger.Info("Starting reconciliation")
 
-	osb := &operatorv1alpha1.OpenShiftBuild{}
-	if err := r.Get(ctx, req.NamespacedName, osb); err != nil {
-		if errors.IsNotFound(err) {
-			logger.Info("Resource not found")
-			return ctrl.Result{Requeue: false}, nil
-		}
-		logger.Error(err, "Retrieving object from cache")
+	// Create OpenShiftBuild resource if missing
+	openShiftBuild := &openshiftv1alpha1.OpenShiftBuild{
+		ObjectMeta: metav1.ObjectMeta{Name: req.Name},
+	}
+	if result, err := r.CreateOrUpdate(ctx, openShiftBuild); err != nil {
+		logger.Error(err, "Failed to create or update resource")
+		return ctrl.Result{}, err
+	} else if result != controllerutil.OperationResultNone {
+		logger.Info(fmt.Sprintf("Resource %s", result))
 		return ctrl.Result{}, nil
 	}
 
-	// TODO(user): Import upstream Shipwright Operator Reconcile method
+	// Initialize status
+	if openShiftBuild.Status.Conditions == nil {
+		openShiftBuild.Status.Conditions = []metav1.Condition{}
+		apimeta.SetStatusCondition(&openShiftBuild.Status.Conditions, metav1.Condition{
+			Type:    openshiftv1alpha1.ConditionReady,
+			Status:  metav1.ConditionUnknown,
+			Reason:  "Initializing",
+			Message: "Initializing Openshift Builds Operator",
+		})
+		if err := r.Client.Status().Update(ctx, openShiftBuild); err != nil {
+			logger.Error(err, "Failed to initialize status")
+			return ctrl.Result{}, err
+		}
+	}
 
+	// TODO: Create ShipwrightBuild CR
+
+	// Update status
+	apimeta.SetStatusCondition(&openShiftBuild.Status.Conditions, metav1.Condition{
+		Type:    openshiftv1alpha1.ConditionReady,
+		Status:  metav1.ConditionTrue,
+		Reason:  "Success",
+		Message: "Successfully reconciled OpenShiftBuild",
+	})
+	if err := r.Client.Status().Update(ctx, openShiftBuild); err != nil {
+		logger.Error(err, "Failed to update status")
+		return ctrl.Result{}, err
+	}
+
+	logger.Info("Finished reconciliation")
 	return ctrl.Result{}, nil
+}
+
+// CreateOrUpdate will create or update v1alpha1.OpenShiftBuild resource
+func (r *OpenShiftBuildReconciler) CreateOrUpdate(ctx context.Context, object *openshiftv1alpha1.OpenShiftBuild) (controllerutil.OperationResult, error) {
+	return ctrl.CreateOrUpdate(ctx, r.Client, object, func() error {
+		if !controllerutil.ContainsFinalizer(object, common.OpenShiftBuildFinalizerName) {
+			controllerutil.AddFinalizer(object, common.OpenShiftBuildFinalizerName)
+		}
+		if object.Spec.Shipwright == nil {
+			object.Spec.Shipwright = &openshiftv1alpha1.Shipwright{
+				Build: &openshiftv1alpha1.ShipwrightBuild{
+					State: openshiftv1alpha1.Enabled,
+				},
+			}
+		}
+		if object.Spec.SharedResource == nil {
+			object.Spec.SharedResource = &openshiftv1alpha1.SharedResource{
+				State: openshiftv1alpha1.Enabled,
+			}
+		}
+		return nil
+	})
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *OpenShiftBuildReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Check if OpenShiftBuild CRD is present
+	CRDClient, err := apiextensionsv1.NewForConfig(mgr.GetConfig())
+	if err != nil {
+		return err
+	}
+	_, err = CRDClient.CustomResourceDefinitions().Get(context.TODO(), common.OpenShiftBuildCRDName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Replace the client with a non-cached client to create resource before starting manager
+	client, err := client.New(mgr.GetConfig(), client.Options{Scheme: r.Scheme})
+	if err != nil {
+		return err
+	}
+
+	// Create OpenShitBuild CR if not present
+	list := openshiftv1alpha1.OpenShiftBuildList{}
+	if err := client.List(context.TODO(), &list); err != nil {
+		return err
+	}
+	if len(list.Items) == 0 {
+		if err := client.Create(context.TODO(), &openshiftv1alpha1.OpenShiftBuild{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: common.OpenShiftBuildResourceName,
+			},
+		}); err != nil {
+			return err
+		}
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&operatorv1alpha1.OpenShiftBuild{}).
+		For(&openshiftv1alpha1.OpenShiftBuild{}).
+		Owns(&shipwrightv1alpha1.ShipwrightBuild{}).
+		WithEventFilter(predicate.Funcs{
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				return e.ObjectOld.GetGeneration() != e.ObjectNew.GetGeneration()
+			},
+			DeleteFunc: func(e event.DeleteEvent) bool {
+				return !e.DeleteStateUnknown
+			},
+		}).
 		Complete(r)
 }
---
+++ internal/controller/shipwrightbuild_controller.go
@@ -0,0 +1,85 @@
+package controller
+
+import (
+	"context"
+	"os"
+
+	manifestivalclient "github.com/manifestival/controller-runtime-client"
+	"github.com/manifestival/manifestival"
+	openshiftv1alpha1 "github.com/redhat-openshift-builds/operator/api/v1alpha1"
+	"github.com/redhat-openshift-builds/operator/internal/common"
+	shipwrightv1alpha1 "github.com/shipwright-io/operator/api/v1alpha1"
+	shipwrightoperator "github.com/shipwright-io/operator/controllers"
+	tektonoperatorv1alpha1 "github.com/tektoncd/operator/pkg/client/clientset/versioned/typed/operator/v1alpha1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+type ShipwrightBuildReconciler shipwrightoperator.ShipwrightBuildReconciler
+
+func (r *ShipwrightBuildReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Create Owner Reference for filtering
+	gvk, err := r.Client.GroupVersionKindFor(&openshiftv1alpha1.OpenShiftBuild{})
+	if err != nil {
+		return err
+	}
+	owner := &metav1.OwnerReference{
+		APIVersion: gvk.GroupVersion().String(),
+		Kind:       gvk.Kind,
+	}
+
+	// Initialize logger
+	r.Logger = mgr.GetLogger()
+
+	// Initialize CRD client from REST config
+	if r.CRDClient, err = apiextensionsv1.NewForConfig(mgr.GetConfig()); err != nil {
+		return err
+	}
+
+	// Initialize Tekton Operator client from REST config
+	if r.TektonOperatorClient, err = tektonoperatorv1alpha1.NewForConfig(mgr.GetConfig()); err != nil {
+		return err
+	}
+
+	// Initialize Manifest
+	manifestPath := common.ShipwrightBuildManifestPath
+	if path, ok := os.LookupEnv(common.ShipwrightManifestPathEnv); ok {
+		manifestPath = path
+	}
+	manifestivalOptions := []manifestival.Option{
+		manifestival.UseLogger(r.Logger),
+		manifestival.UseClient(manifestivalclient.NewClient(mgr.GetClient())),
+	}
+	if r.Manifest, err = manifestival.NewManifest(manifestPath, manifestivalOptions...); err != nil {
+		return err
+	}
+
+	_, err = r.CRDClient.CustomResourceDefinitions().Get(context.TODO(), common.OpenShiftBuildCRDName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	reconciler := shipwrightoperator.ShipwrightBuildReconciler(*r)
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&shipwrightv1alpha1.ShipwrightBuild{}).
+		WithEventFilter(predicate.Funcs{
+			CreateFunc: func(e event.CreateEvent) bool {
+				return common.IsControlledBy(e.Object, owner)
+			},
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				return common.IsControlledBy(e.ObjectOld, owner) &&
+					common.IsControlledBy(e.ObjectNew, owner) &&
+					!controllerutil.ContainsFinalizer(e.ObjectNew, common.OpenShiftBuildFinalizerName) &&
+					!e.ObjectNew.GetDeletionTimestamp().IsZero()
+			},
+			DeleteFunc: func(e event.DeleteEvent) bool {
+				return false
+			},
+		}).
+		Complete(&reconciler)
+}
